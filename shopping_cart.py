358379d802737e83c3f386ce2a0656a69bead1fc
daecker <50798284+daecker@users.noreply.github.com>
2019-06-15 08:46:43 -0400

    Assemble Running Total Price

--- shopping_cart.py
+++ shopping_cart.py
@@ -32,6 +32,8 @@ products = [
 
 #Step 1: Capture product IDs
 
+total_price = 0
+
 while True:
     selected_id = input("Please input a product identifier: ") #output is a string
 
@@ -40,12 +42,16 @@ while True:
     else:
         matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
         matching_product = matching_products[0]
+        total_price = total_price + matching_product["price"]
         print("SELECTED PRODUCT: " + str(matching_product["name"]) + str(matching_product["price"]))
 
 
 
 #Step 2: Calculations on products
+
 #Step 3: Print Results
+
+print("TOTAL PRICE: " + str(total_price))
 
 
 #END RESULT
