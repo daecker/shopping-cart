09f690a6a0429c17e51c38a8f442cfb683503ea3
daecker <50798284+daecker@users.noreply.github.com>
2019-06-15 08:59:34 -0400

    Update Order of Operations

--- shopping_cart.py
+++ shopping_cart.py
@@ -33,6 +33,7 @@ products = [
 #Step 1: Capture product IDs
 
 total_price = 0
+selected_ids = []
 
 while True:
     selected_id = input("Please input a product identifier: ") #output is a string
@@ -40,16 +41,18 @@ while True:
     if selected_id == "DONE":
         break
     else:
-        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-        matching_product = matching_products[0]
-        total_price = total_price + matching_product["price"]
-        print("SELECTED PRODUCT: " + str(matching_product["name"]) + str(matching_product["price"]))
-
+        selected_ids.append(selected_id)
 
 
 #Step 2: Calculations on products
 
 #Step 3: Print Results
+
+for selected_id in selected_ids:
+    matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+    matching_product = matching_products[0]
+    total_price = total_price + matching_product["price"]
+    print("SELECTED PRODUCT: " + str(matching_product["name"]) + " " + str(matching_product["price"]))
 
 print("TOTAL PRICE: " + str(total_price))
 
