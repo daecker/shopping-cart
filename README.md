61ea0e31379b3f3ba8db30e88fa8696e626242c5
daecker <50798284+daecker@users.noreply.github.com>
2019-06-15 08:10:14 -0400

    Create User Input

--- README.md
+++ README.md
@@ -67,6 +67,7 @@ From within the virtual environment, install the pytest package:
 pip install pytest
 ```
 
+# Usage
 From within the virtual environment, demonstrate your ability to run the Python script from the command-line:
 
 ```sh
--- shopping_cart.py
+++ shopping_cart.py
@@ -25,7 +25,49 @@ products = [
     {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
 ] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 
-print(products)
+#print(products)
 # pprint(products)
 
 # TODO: write some Python code here to produce the desired output
+
+#Step 1: Capture product IDs
+
+selected_id = input("Please input a product identifier:") #output is a string
+print(selected_id)
+print(type(selected_id))
+
+#Step 2: Calculations on products
+#Step 3: Print Results
+
+
+#END RESULT
+# #(shopping-env)  --->> python shopping_cart.py
+#Please input a product identifier: 1
+#Please input a product identifier: 8
+#Please input a product identifier: 6
+#Please input a product identifier: 8
+#Please input a product identifier: 8
+#Please input a product identifier: 16
+#Please input a product identifier: 12
+#Please input a product identifier: DONE
+##> ---------------------------------
+##> GREEN FOODS GROCERY
+##> WWW.GREEN-FOODS-GROCERY.COM
+##> ---------------------------------
+##> CHECKOUT AT: 2019-06-06 11:31 AM
+##> ---------------------------------
+##> SELECTED PRODUCTS:
+##>  ... Chocolate Sandwich Cookies ($3.50)
+##>  ... Cut Russet Potatoes Steam N' Mash ($4.25)
+##>  ... Dry Nose Oil ($21.99)
+##>  ... Cut Russet Potatoes Steam N' Mash ($4.25)
+##>  ... Cut Russet Potatoes Steam N' Mash ($4.25)
+##>  ... Mint Chocolate Flavored Syrup ($4.50)
+##>  ... Chocolate Fudge Layer Cake ($18.50)
+##> ---------------------------------
+##> SUBTOTAL: $61.24
+##> TAX: $5.35
+##> TOTAL: $66.59
+##> ---------------------------------
+##> THANKS, SEE YOU AGAIN SOON!
+##> ---------------------------------
